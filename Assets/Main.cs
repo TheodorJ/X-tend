708b84845ba9348cd2efe23e8a964cb762a82fc7
TheodorJ <teddyjohansson@live.com>
2018-02-09 19:33:45 -0500

    Created platform object

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main.cs : MonoBehaviour {
+public class Main : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
